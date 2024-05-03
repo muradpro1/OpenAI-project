cedca381f85287f3ebcfda4f35400a2a1fd074a4
Murad Bayramli <muradbayram.eng@gmail.com>
2024-05-03 10:54:15 +0400

    Horizontal scrolldown removed

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-console.log(document.querySelector(header))
-while(5==5){
-    const header=document.querySelector(header)
-    if(window.scrollY==0){
-        
-    }
-}
+const linesContainer = document.querySelector('.lines-js')
+  let whiteHeight = 0
+  let blackHeight = 20
+
+  function createZebra(wh, bh) {
+    console.log('here', wh, bh);
+    const markup = `
+      <div class="zebra">
+        <p class="white" style="height: ${wh}px"></p>
+        <p class="black" style="height: ${bh}px"></p>
+      </div>
+    `
+    linesContainer.insertAdjacentHTML('beforeend', markup)
+  }
+  
+  for(let i = 0; i < 20; i++) {
+    whiteHeight++
+    blackHeight--
+    createZebra(whiteHeight, blackHeight)
+  }
